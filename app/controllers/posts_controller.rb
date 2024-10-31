3fc185dfa1f81c019cd0a0a0d65512b059ab0843
Benoit GILLIOT <b.gilliot@gmail.com>
2024-10-31 20:13:11 +0100

    index view 80%

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: [:index]
+  
   def index
-    @posts = Post.all
+    @posts = Post.all.order(score: :desc).first(30)
   end
 end
